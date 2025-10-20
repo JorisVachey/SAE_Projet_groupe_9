98679f206b960a9f0154797e1b58355c7113bc63
gboissel <gauthier.boissel@etu.univ-orleans.fr>
2025-10-20 19:53:03 +0200

    preparation des branches pour implementation du proje en flask

---
+++ config.py
@@ -0,0 +1,8 @@
+import random, string, os
+#>>>"".join([random.choice(string.printable) for _ in os.urandom(24) ] )
+SECRET_KEY = "2lzUl{$*D6#`8uXqlU."
+ABOUT = "Bienvenue sur la page à propos de Flask !"
+CONTACT = "page de contact"
+BOOTSTRAP_SERVE_LOCAL = True
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'mariadb:///' + os.path.join(basedir, 'monApp.db')
---
+++ monApp/views.py
@@ -0,0 +1,13 @@
+from .app import app
+# from config import ABOUT,CONTACT
+from flask import render_template,request,redirect,url_for
+# from flask_login import logout_user,login_user,login_required
+# from monApp.forms import *
+@app.route('/')
+@app.route('/index/')
+def index():
+    if len(request.args)==0:
+        return render_template("index.html",title ="R3.01 Dev Web avec Flask",name="")
+    else :
+        param_name = request.args.get('name')
+        return render_template("index.html",title="R3.01 Dev Web avec Flask",name=param_name)
