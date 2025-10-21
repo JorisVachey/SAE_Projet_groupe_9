f17523100cd5568a2e8d0dee486ccf0aabfbda9d
EtienneA20 <etienne.audor@etu.univ-orleans.fr>
2025-10-21 13:06:54 +0200

    fin du modele, avec trigger et les tests pour chacun

--- testTrigger.sql
+++ testTrigger.sql
@@ -9,7 +9,7 @@ il complete insertTest.sql
 --(5, 1, '2025-10-25', 11, true, 'confirmée');
 --MariaDB [DBaudor]> INSERT INTO RESERVATION (idR, idCli, dateR, nb_couverts, sur_place, statut) VALUES
 --    -> (5, 1, '2025-10-25', 11, true, 'confirmée');
---ERROR 1644 (45000): Pour cette journée, il y a deja 3 couverts de reserver, 
+--ERROR 1644 (45000): Pour cette journée, il y a deja 3 couverts de reserver,
 --Merci de repasser demain ou de 'commander a emporter'
 
 -- --------------------------------
@@ -31,9 +31,130 @@ il complete insertTest.sql
 --(7, 3, '2025-10-26', 2, false, 'en attente');
 --MariaDB [DBaudor]> INSERT INTO RESERVATION (idR, idCli, dateR, nb_couverts, sur_place, statut) VALUES
 --    -> (7, 3, '2025-10-26', 2, false, 'en attente');
---ERROR 1644 (45000): Vous etes sur la liste noir, il est impossible pour vous de commander en ligne, 
+--ERROR 1644 (45000): Vous etes sur la liste noir, il est impossible pour vous de commander en ligne,
 --nous vous invitons tout de meme a commander sur place
 
 -- -------------------------------
--- Test d'échec : trigger verifDisponibiliterPlat
+-- Test de reussite : trigger verifDisponibiliterPlat 1er cas (sur_place)
 -- -------------------------------
+--INSERT INTO PLAT (idP, nomP, typeP, prixP, stock, stockInit, descriptionP) VALUES
+--(7, 'roti de test', 'Plat', 18.5, 10,0 , 'boeuf, chataigne, vin blanc');
+--INSERT INTO FORMULE (idF, nomF, prixF) VALUES
+--(4, 'menu viande',25);
+--INSERT INTO COMPOSER (idF, idP, quantiteC) VALUES
+--(4, 7, 2);
+--INSERT INTO RESERVATION (idR, idCli, dateR, nb_couverts, sur_place, statut) VALUES
+--(5, 1, '2025-10-26', 2, true, 'en attente');
+--INSERT INTO CONTENIR_F (idR, idF, quantiteF) VALUES
+--(5, 4, 2);
+--On constate la modification de stock:
+--MariaDB [DBaudor]> select * from PLAT where idP=7;
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--| idP | nomP         | typeP | prixP | stock | stockInit | descriptionP                |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--|   7 | roti de test | Plat  | 18.50 |     6 |        10 | boeuf, chataigne, vin blanc |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--INSERT INTO CONTENIR_P (idR, idP, quantiteP) VALUES
+--(5, 7, 6);
+--MariaDB [DBaudor]> select * from PLAT where idP=7;
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--| idP | nomP         | typeP | prixP | stock | stockInit | descriptionP                |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--|   7 | roti de test | Plat  | 18.50 |     0 |        10 | boeuf, chataigne, vin blanc |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+
+--On constate que l'on ne peut plus ajouter de plat
+--UPDATE CONTENIR_P set quantiteP = quantiteP+1 where idP=7 and idR=5;
+--MariaDB [DBaudor]> UPDATE CONTENIR_P set quantiteP = quantiteP+1 where idP=7 and idR=5;
+--ERROR 1644 (45000): Le plat d'id 7 n'a pas assez de stock pour votre commande.
+--Il reste 0 unités en stock, et vous demandez 1 en plus .
+--Merci de revoir votre commande.
+
+--On constate que l'on peut enlever un plat
+--UPDATE CONTENIR_P set quantiteP = quantiteP-1 where idP=7 and idR=5;
+--MariaDB [DBaudor]> select * from PLAT where idP=7;
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--| idP | nomP         | typeP | prixP | stock | stockInit | descriptionP                |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--|   7 | roti de test | Plat  | 18.50 |     1 |        10 | boeuf, chataigne, vin blanc |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+
+--on constate que l'on peut supprimer tout les plats
+--delete from  CONTENIR_P where idP=7 and idR=5;
+--MariaDB [DBaudor]> select * from PLAT where idP=7;
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--| idP | nomP         | typeP | prixP | stock | stockInit | descriptionP                |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--|   7 | roti de test | Plat  | 18.50 |     6 |        10 | boeuf, chataigne, vin blanc |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+
+--On constate que l'on ne peut ajouter des formules
+--UPDATE CONTENIR_F set quantiteF = quantiteF+3 where idF=4 and idR=5;
+--MariaDB [DBaudor]> select * from PLAT where idP=7;
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--| idP | nomP         | typeP | prixP | stock | stockInit | descriptionP                |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--|   7 | roti de test | Plat  | 18.50 |     0 |        10 | boeuf, chataigne, vin blanc |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+
+--On constate que l'on ne peut plus ajouter de formule;
+--UPDATE CONTENIR_F set quantiteF = quantiteF+1 where idF=4 and idR=5;
+--MariaDB [DBaudor]> UPDATE CONTENIR_F set quantiteF = quantiteF+1 where idF=4 and idR=5;
+--ERROR 1644 (45000): Le plat d'id 7 n'a pas assez de stock pour votre commande.
+--Il reste 0 unités en stock, et vous demandez 2 en plus .
+--Merci de revoir votre commande.
+
+--On constate que l'on peut supprimer toutes les formules (ou un certain nombre)
+--delete from  CONTENIR_F where idF=4 and idR=5;
+--MariaDB [DBaudor]> select * from PLAT where idP=7;
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--| idP | nomP         | typeP | prixP | stock | stockInit | descriptionP                |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+--|   7 | roti de test | Plat  | 18.50 |    10 |        10 | boeuf, chataigne, vin blanc |
+--+-----+--------------+-------+-------+-------+-----------+-----------------------------+
+
+
+-- -------------------------------
+-- Test d'échec : trigger verifDisponibiliterPlat 2eme cas (en ligne)
+-- -------------------------------
+
+--INSERT INTO PLAT (idP, nomP, typeP, prixP, stock, stockInit, descriptionP) VALUES
+--(8, 'dinde de test', 'Plat', 15.5, 10,0 , 'dinde, epice , pomme de terre');
+--INSERT INTO FORMULE (idF, nomF, prixF) VALUES
+--(5, 'menu volaille',20);
+--INSERT INTO COMPOSER (idF, idP, quantiteC) VALUES
+--(5, 8, 2);
+--INSERT INTO RESERVATION (idR, idCli, dateR, nb_couverts, sur_place, statut) VALUES
+--(6, 1, '2025-10-27', 2, false, 'en attente');
+
+--On constate que l'on ne pas pas commander plus de 80 % du stock en ligne
+--INSERT INTO CONTENIR_P (idR, idP, quantiteP) VALUES
+--(6, 8, 9);
+--MariaDB [DBaudor]> INSERT INTO CONTENIR_P (idR, idP, quantiteP) VALUES
+--    -> (6, 8, 9);
+--ERROR 1644 (45000): Le plat d id 8 n a pas assez de stock pour votre commande.
+--Il reste 10 unités en stock,
+--et vous en demandez 9.
+--Merci de revoir votre commande.
+
+--On constate que celui la fonctionne
+--INSERT INTO CONTENIR_P (idR, idP, quantiteP) VALUES
+--(6, 8, 8);
+
+--On constate que la modification ne focntionne pas
+--update CONTENIR_P set quantiteP = quantiteP + 1 where idP=8 and idR=6
+--MariaDB [DBaudor]> update CONTENIR_P set quantiteP = quantiteP + 1 where idP=8 and idR=6
+--    -> ;
+--ERROR 1644 (45000): Le plat d'id 8 n'a pas assez de stock pour votre commande.
+--Il reste 2 unités en stock, et vous demandez 1 en plus .
+--Merci de revoir votre commande.
+
+--On constate le meme fonctionnement pour les formules: ( j'ai reset le stock a 10)
+--INSERT INTO CONTENIR_F (idR, idF, quantiteF) VALUES
+--(6, 5, 5);
+--MariaDB [DBaudor]> INSERT INTO CONTENIR_F (idR, idF, quantiteF) VALUES
+--    -> (6, 5, 5);
+--ERROR 1644 (45000): Le plat d id 8 n a pas assez de stock pour votre commande.
+--Il reste 10 unités en stock,
+--et vous en demandez 10.
+--Merci de revoir votre commande.
--- trigger.sql
+++ trigger.sql
@@ -30,7 +30,29 @@ permet de verifier pour chaque reservation (sur place ou non)
 si la quantité de plat ne depasse pas le stock du restaurant
 */
 DELIMITER |
-create or replace trigger verifDisponibiliterPlat before insert on RESERVATION for each row
+create or replace trigger verifDisponibiliterPlatUnique before insert on CONTENIR_P for each row
+begin
+    DECLARE mes VARCHAR(500) DEFAULT '';
+    DECLARE variableChoixCom DECIMAL(3,2) DEFAULT 1;
+    DECLARE quantiteStock INT;
+    DECLARE quantiteInit INT;
+    declare place boolean default false;
+    select sur_place into place from RESERVATION where idR=new.idR;
+    SELECT stock, stockInit INTO quantiteStock, quantiteInit
+        FROM PLAT
+        WHERE idP = new.idP;
+    if place=false then
+        set variableChoixCom=0.8;
+    end if;
+    IF new.quantiteP > quantiteStock OR new.quantiteP > quantiteInit * variableChoixCom THEN
+        set mes = concat(mes,"Le plat d id ",new.idP," n a pas assez de stock pour votre commande. \nIl reste ",quantiteStock," unités en stock, \net vous en demandez ",new.quantiteP,".\nMerci de revoir votre commande.");
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = mes;
+    end if ;
+end|
+DELIMITER ;
+
+DELIMITER |
+create or replace trigger verifDisponibiliterPlatUniqueUpdate before UPDATE on CONTENIR_P for each row
 begin
     declare mes varchar(500) default '';
     declare variableChoixCom DECIMAL(3,2) default 1;
@@ -39,21 +61,73 @@ begin
     declare quantiteInit int;
     declare qteDemande int;
     declare fini boolean default false;
-    declare platUnique cursor for
-        SELECT idP, SUM(quantite) AS quantite_totale
-        FROM (
-            SELECT idP, quantiteP AS quantite
-            FROM CONTENIR_P
-            WHERE idR = new.idR
-            UNION ALL
-            SELECT idP, (quantiteF * quantiteC) AS quantite
-            FROM CONTENIR_F
-            NATURAL JOIN COMPOSER
-            WHERE idR = new.idR
-        ) AS q
-        GROUP BY idP;
+    declare place boolean default false;
+    select sur_place into place from RESERVATION where idR=new.idR;
+    if place=false then
+        set variableChoixCom=0.8;
+    end if;
+    set qteDemande = NEW.quantiteP - OLD.quantiteP;
+    IF qteDemande > 0 THEN
+        SELECT stock, stockInit INTO quantiteStock, quantiteInit
+        FROM PLAT
+        WHERE idP = NEW.idP;
+        IF (qteDemande > quantiteStock or NEW.quantiteP > quantiteInit * variableChoixCom) THEN
+            set mes = CONCAT(
+                "Le plat d'id ", NEW.idP,
+                " n'a pas assez de stock pour votre commande.\nIl reste ",
+                quantiteStock, " unités en stock, et vous demandez ",
+                qteDemande, " en plus .\nMerci de revoir votre commande."
+            );
+            SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = mes;
+        END IF;
+    END IF;
+end|
+DELIMITER ;
+
+/* on met a jour le stock après avoir ajouter,modifier ou supprimer des plats des plats, si ils sont validés
+*/
+DELIMITER |
+CREATE OR REPLACE TRIGGER majStockPlatUnique AFTER INSERT ON CONTENIR_P FOR EACH ROW
+BEGIN
+    UPDATE PLAT SET stock = stock - new.quantiteP WHERE idP = new.idP;
+END|
+DELIMITER ;
+
+DELIMITER |
+CREATE OR REPLACE TRIGGER majStockPlatUniqueUpdate AFTER UPDATE ON CONTENIR_P FOR EACH ROW
+BEGIN
+    UPDATE PLAT SET stock = stock - (new.quantiteP - old.quantiteP) WHERE idP = new.idP;
+END|
+DELIMITER ;
+
+DELIMITER |
+CREATE OR REPLACE TRIGGER majStockPlatUniqueDelete AFTER delete ON CONTENIR_P FOR EACH ROW
+BEGIN
+    UPDATE PLAT SET stock = stock + old.quantiteP WHERE idP = old.idP;
+END|
+DELIMITER ;
+
+/*meme chose pour l'autre table
+*/
+
+DELIMITER |
+create or replace trigger verifDisponibiliterPlatForm before insert on CONTENIR_F for each row
+begin
+    declare mes varchar(500) default '';
+    declare variableChoixCom DECIMAL(3,2) default 1;
+    declare idPlat int;
+    declare quantiteStock int;
+    declare quantiteInit int;
+    declare qteDemande int;
+    declare fini boolean default false;
+    declare place boolean default false;
+    DECLARE platUnique CURSOR FOR
+        SELECT idP, quantiteC * NEW.quantiteF AS qteDemande
+        FROM COMPOSER
+        WHERE idF = NEW.idF;
     declare continue HANDLER for not found set fini = TRUE;
-    if new.sur_place=false then
+    select sur_place into place from RESERVATION where idR=new.idR;
+    if place=false then
         set variableChoixCom=0.8;
     end if;
     open platUnique ;
@@ -61,7 +135,7 @@ begin
         if not fini then
             fetch platUnique into idPlat,qteDemande ;
             select stock, stockInit into quantiteStock, quantiteInit from PLAT where idP=idPlat ;
-            if (qteDemande > quantiteInit*variableChoixCom OR qteDemande>quantiteStock) then
+            IF qteDemande > quantiteStock or qteDemande > quantiteInit * variableChoixCom then
                 set mes = concat(mes,"Le plat d id ",idPlat," n a pas assez de stock pour votre commande. \nIl reste ",quantiteStock," unités en stock, \net vous en demandez ",qteDemande,".\nMerci de revoir votre commande.");
                 SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = mes;
             end if ;
@@ -69,6 +143,80 @@ begin
     end while ;
     close platUnique ;
 end|
+DELIMITER ;
+
+DELIMITER |
+create or replace trigger verifDisponibiliterPlatFormUpdate before UPDATE on CONTENIR_F for each row
+begin
+    declare mes varchar(500) default '';
+    declare variableChoixCom DECIMAL(3,2) default 1;
+    declare idPlat int;
+    declare quantiteStock int;
+    declare quantiteInit int;
+    declare qteDemande int;
+    declare fini boolean default false;
+    declare place boolean default false;
+    declare qteEnCours int default 0;
+    declare qteAncien CURSOR for
+        SELECT idP, old.quantiteF * quantiteC AS qte_diff
+        FROM COMPOSER
+        WHERE idF = new.idF;
+    declare platUnique CURSOR FOR
+        SELECT idP, (new.quantiteF - old.quantiteF) * quantiteC AS qte_diff
+        FROM COMPOSER
+        WHERE idF = new.idF;
+    declare continue HANDLER for not found set fini = TRUE;
+    select sur_place into place from RESERVATION where idR=new.idR;
+    if place=false then
+        set variableChoixCom=0.8;
+    end if;
+    open platUnique ;
+    while not fini do
+        if not fini then
+            fetch platUnique into idPlat,qteDemande ;
+            fetch qteAncien into qteEnCours;
+            IF qteDemande > 0 THEN
+                SELECT stock, stockInit INTO quantiteStock, quantiteInit
+                FROM PLAT
+                WHERE idP = idPlat;
+                IF (qteDemande > quantiteStock or qteDemande+qteEnCours > quantiteInit * variableChoixCom) THEN
+                    set mes = CONCAT(
+                        "Le plat d'id ", idPlat,
+                        " n'a pas assez de stock pour votre commande.\nIl reste ",
+                        quantiteStock, " unités en stock, et vous demandez ",
+                        qteDemande, " en plus .\nMerci de revoir votre commande."
+                    );
+                    SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = mes;
+                END IF;
+            END IF;
+        end if;
+    end while;
+    close platUnique;
+end|
+DELIMITER ;
+
+/*meme chose avec les formules
+*/
+
+DELIMITER |
+CREATE OR REPLACE TRIGGER majStockPlatForm AFTER INSERT ON CONTENIR_F FOR EACH ROW
+BEGIN
+    UPDATE PLAT NATURAL JOIN COMPOSER SET stock = stock - (new.quantiteF * quantiteC) WHERE idF = new.idF;
+END|
+DELIMITER ;
+
+DELIMITER |
+CREATE OR REPLACE TRIGGER majStockPlatFormUpdate AFTER UPDATE ON CONTENIR_F FOR EACH ROW
+BEGIN
+    UPDATE PLAT NATURAL JOIN COMPOSER SET stock = stock - ((new.quantiteF - old.quantiteF) * quantiteC) WHERE idF = new.idF;
+END|
+DELIMITER ;
+
+DELIMITER |
+CREATE OR REPLACE TRIGGER majStockPlatFormDelete AFTER delete ON CONTENIR_F FOR EACH ROW
+BEGIN
+    UPDATE PLAT NATURAL JOIN COMPOSER SET stock = stock + (old.quantiteF * quantiteC) WHERE idF = old.idF;
+END|
 DELIMITER ;
 
 
