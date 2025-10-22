b7b2e0988dbf60c1025d51c25c59cf75891a87e9
Nolan405 <nolan.morain@etu.univ-orleans.fr>
2025-10-22 13:58:38 +0200

    debut flask

--- monApp/views.py
+++ monApp/views.py
@@ -1,13 +1,34 @@
 from .app import app
-# from config import ABOUT,CONTACT
-from flask import render_template,request,redirect,url_for
-# from flask_login import logout_user,login_user,login_required
-# from monApp.forms import *
+from flask import render_template
+
 @app.route('/')
 @app.route('/index/')
-def index():
-    if len(request.args)==0:
-        return render_template("index.html",title ="R3.01 Dev Web avec Flask",name="")
-    else :
-        param_name = request.args.get('name')
-        return render_template("index.html",title="R3.01 Dev Web avec Flask",name=param_name)
+def index() :
+    return render_template("index.html", name="Cricri")
+
+@app.route('/propos/')
+def propos() :
+    return "page a propos"
+
+@app.route('/menu/')
+def menu() :
+    return "page menu"
+
+@app.route('/contact/')
+def contact() :
+    return "page contact"
+
+@app.route('/nouvautes/')
+def nouvautes() :
+    return "page nouvautes"
+
+@app.route('/connection/')
+def connection() :
+    return "page connection"
+
+@app.route('/inscritpion/')
+def inscritpion() :
+    return "page inscritpion"
+    
+if __name__== "__main__" :
+    app.run()
