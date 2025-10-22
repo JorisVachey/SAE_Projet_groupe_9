46cf7b42d392634b60fd2b0b978cfee440ea3f98
Malo-Dantec <mdantec56@gmail.com>
2025-10-22 17:22:46 +0200

    update liens si connecté, login

--- monApp/views.py
+++ monApp/views.py
@@ -1,5 +1,8 @@
-from .app import app
-from flask import render_template
+from requests import session
+from .app import app, db
+from flask import render_template, redirect, url_for
+from flask_login import login_user, logout_user
+from .models import Client, Restauratrice
 
 @app.route('/')
 @app.route('/index/')
@@ -26,13 +29,37 @@ def nouvautes() :
 def connection() :
     from .forms import LoginForm
     connection_form = LoginForm()
+    unUser = None
+    if connection_form.validate_on_submit():
+        unUser = connection_form.get_authenticated_user()
+        if unUser:
+            login_user(unUser)
+            if isinstance(unUser, Client):
+                session['user_type'] = 'client'
+                return redirect(url_for('index'))
+            elif isinstance(unUser, Restauratrice):
+                session['user_type'] = 'restauratrice'
+                # return redirect(url_for('admin'))
+                return redirect(url_for('index')) # remplacer par admin lorsque implémenté
     return render_template("connection.html", form=connection_form)
+
+@app.route('/deconnection/')
+def deconnection() :
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/inscription/')
 def inscription():
     from .forms import RegisterForm
     inscription_form = RegisterForm()
+    newUser = None
+    if inscription_form.validate_on_submit():
+        newUser = inscription_form.get_registered_user()
+        if newUser:
+            db.session.add(newUser)
+            db.session.commit()
+            return redirect(url_for('connection'))
     return render_template("inscription.html", form=inscription_form)
-    
+
 if __name__== "__main__" :
     app.run()
