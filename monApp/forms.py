8ebd858ab891d38a107ad8f7c95e26339121243d
Malo-Dantec <mdantec56@gmail.com>
2025-10-22 16:14:49 +0200

    validators forms

--- monApp/forms.py
+++ monApp/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import Form, StringField
+from wtforms.validators import DataRequired
 # from .models import Client, Restauratrice
 
 class RegisterForm(FlaskForm):
-    numtel = StringField('Numéro de téléphone')
-    pseudonyme = StringField('Pseudonyme')
-    password = StringField('Mot de passe')
+    numtel = StringField('Numéro de téléphone', validators=[DataRequired()])
+    pseudonyme = StringField('Pseudonyme', validators=[DataRequired()])
+    password = StringField('Mot de passe', validators=[DataRequired()])
 
 class LoginForm(FlaskForm):
-    numtel = StringField('Numéro de téléphone')
-    password = StringField('Mot de passe')
-    
+    numtel = StringField('Numéro de téléphone', validators=[DataRequired()])
+    password = StringField('Mot de passe', validators=[DataRequired()])
