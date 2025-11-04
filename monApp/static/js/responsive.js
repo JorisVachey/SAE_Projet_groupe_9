305f2204ef73d1653f0cc3ae1e08d1e12dd2d340
Nolan405 <nolan.morain@etu.univ-orleans.fr>
2025-11-04 16:41:48 +0100

    fin responsive header pageAccueil footer

--- monApp/static/js/responsive.js
+++ monApp/static/js/responsive.js
@@ -1,6 +1,13 @@
 const btnHamburger = document.getElementById('btn_hamburger');
 const menu = document.getElementById('menu');
 
+const logo = document.getElementById('logo_hamburger');
+let toggle = false;
+const imgCroix = logo.dataset.croix;
+const imgHamburger = logo.dataset.hamburger;
+
 btnHamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
+    toggle = !toggle;
+    logo.src = toggle ? imgCroix : imgHamburger;
 });
