1d3a6682d47368f2e9732a0a4363a62431fece73
Malo-Dantec <mdantec56@gmail.com>
2025-11-05 10:37:34 +0100

    ajout page nouveautes

--- monApp/views.py
+++ monApp/views.py
@@ -86,9 +86,9 @@ def contact() :
     return render_template("contact.html")
 
 
-@app.route('/nouvautes/')
-def nouvautes() :
-    return "page nouvautes"
+@app.route('/nouveautes/')
+def nouveautes() :
+    return render_template("nouveautes.html")
 
 @app.route('/connection/', methods=("GET","POST",))
 def connection() :
