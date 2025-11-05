486052d7477155573b284278ff955a8d6c3ad5f0
EtienneA20 <etienne.audor@etu.univ-orleans.fr>
2025-11-05 11:04:26 +0100

    avancer panier back-end

--- monApp/views.py
+++ monApp/views.py
@@ -1,8 +1,9 @@
 from .app import app, db, mail
 from flask import render_template, redirect, url_for,request,flash, abort
 from flask_login import login_user, logout_user, login_required, current_user
-from monApp.models import db, User, Type_plat, Plat
+from monApp.models import db, User, Type_plat, Plat, Reservation, ContenirP, ContenirF, Formule
 from flask_mail import Mail,Message
+from datetime import date
 import os
 from functools import wraps
 
@@ -111,9 +112,21 @@ def deconnection() :
     return redirect(url_for('index'))
 
 @login_required
-@app.route('/panier/')
-def panier() :
-    return render_template("panier.html")
+def get_or_create_panier(numtelUser):
+    panier = Reservation.query.filter_by(numtelUser=numtelUser, statut="panier").first()
+    if not panier:
+        panier = Reservation(
+            idR=None,
+            numtelUser=numtelUser,
+            dateR=None,
+            nb_couverts=1,
+            sur_place=False,
+            statut="panier"
+        )
+        db.session.add(panier)
+        db.session.commit()
+    return panier
+
 
 @app.route('/inscription/', methods=("GET","POST",))
 def inscription():
@@ -127,6 +140,217 @@ def inscription():
             db.session.commit()
             return redirect(url_for('connection'))
     return render_template("inscription.html", form=inscription_form)
+
+
+@login_required
+@app.route('/panier/')
+def afficher_panier():
+    numtel = current_user.numtelUser
+    panier = get_or_create_panier(numtel)
+    plats = ContenirP.query.filter_by(idR=panier.idR).all()
+    formules = ContenirF.query.filter_by(idR=panier.idR).all()
+    return render_template("panier.html", panier=panier, plats=plats, formules=formules)
+
+@login_required
+@app.route("/panier/ajouter-plat", methods=["POST"])
+def ajouter_panier(idP):
+    """créé un panier ou le recupere par rapport a l'utilisateur connecté
+
+    Args:
+        idP (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    reservation = Reservation.query.filter_by(numtelUser=current_user.numtelUser, statut="en attente").first()
+    if not reservation:
+        reservation = Reservation(
+            idR=None,
+            numtelUser=current_user.numtelUser,
+            dateR=date.today(),
+            nb_couverts=1,
+            sur_place=False,
+            statut="en attente"
+        )
+        db.session.add(reservation)
+        db.session.commit()
+    plat = Plat.query.get(idP)
+    if not plat:
+        flash("Ce plat n’existe pas.", "error")
+        return redirect(url_for("menu"))
+    item = ContenirP.query.filter_by(idR=reservation.idR, idP=idP).first()
+    if item:
+        item.quantiteP += 1
+    else:
+        item = ContenirP(idR=reservation.idR, idP=idP, quantiteP=1)
+        db.session.add(item)
+
+    db.session.commit()
+    flash(f"{plat.nomP} ajouté au panier !", "success")
+    return redirect(url_for("voir_panier"))
+
+@login_required
+@app.route("/ajouter_formule/<int:idF>", methods=["POST"])
+def ajouter_formule(idF):
+    reservation = Reservation.query.filter_by(numtelUser=current_user.numtelUser, statut="en attente").first()
+    if not reservation:
+        reservation = Reservation(
+            idR=None,
+            numtelUser=current_user.numtelUser,
+            dateR=date.today(),
+            nb_couverts=1,
+            sur_place=False,
+            statut="en attente"
+        )
+        db.session.add(reservation)
+        db.session.commit()
+    formule = Formule.query.get(idF)
+    if not formule:
+        flash("Cette formule n’existe pas.", "error")
+        return redirect(url_for("menu"))
+    item = ContenirF.query.filter_by(idR=reservation.idR, idF=idF).first()
+    if item:
+        item.quantiteF += 1
+    else:
+        item = ContenirF(idR=reservation.idR, idF=idF, quantiteF=1)
+        db.session.add(item)
+
+    db.session.commit()
+    flash(f"{formule.nomF} ajoutée au panier !", "success")
+    return redirect(url_for("voir_panier"))
+
+@app.route("/panier/modifier-plat", methods=["POST"])
+@login_required
+def modifier_plat():
+    data = request.get_json()
+    idP = int(data["idP"])
+    quantite = int(data["quantite"])
+
+    panier = get_or_create_panier(current_user.numtelUser)
+    cp = ContenirP.query.filter_by(idR=panier.idR, idP=idP).first()
+
+    if not cp:
+        return jsonify({"error": "Plat non présent dans le panier"}), 404
+
+    if quantite <= 0:
+        db.session.delete(cp)
+    else:
+        cp.quantiteP = quantite
+
+    db.session.commit()
+    return jsonify({"success": True})
+
+@login_required
+@app.route("/modifier_quantite_plat/<int:idP>/<action>", methods=["POST"])
+def modifier_quantite_plat(idP, action):
+    """commande pour interagir avec les bouton + et - du panier
+
+    Args:
+        idP (_type_): _description_
+        action (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    reservation = Reservation.query.filter_by(
+        numtelUser=current_user.numtelUser, statut="en attente"
+    ).first()
+    if not reservation:
+        flash("Aucune réservation en cours.", "error")
+        return redirect(url_for("voir_panier"))
+
+    item = ContenirP.query.filter_by(idR=reservation.idR, idP=idP).first()
+    if not item:
+        flash("Ce plat n’est pas dans votre panier.", "error")
+        return redirect(url_for("voir_panier"))
+
+    plat = Plat.query.get(idP)
+    variableChoixCom = 1
+    if not reservation.sur_place :
+        variableChoixCom= 0.8
+
+    if action == "ajouter":
+        nouvelle_quantite = item.quantiteP + 1
+        if nouvelle_quantite > plat.stock or nouvelle_quantite > plat.stockInit * variableChoixCom:
+            flash(f"Pas assez de stock pour '{plat.nomP}'", "error")
+            return redirect(url_for("voir_panier"))
+        item.quantiteP = nouvelle_quantite
+
+    elif action == "diminuer":
+        item.quantiteP -= 1
+        if item.quantiteP <= 0:
+            db.session.delete(item)  # supprime le plat si la quantité est 0
+
+    db.session.commit()
+    return redirect(url_for("voir_panier"))
+
+@login_required
+@app.route("/modifier_quantite_formule/<int:idF>/<action>", methods=["POST"])
+def modifier_quantite_formule(idF, action):
+    """commande pour interagir avec les bouton + et - du panier
+
+    Args:
+        idP (_type_): _description_
+        action (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    reservation = Reservation.query.filter_by(
+        numtelUser=current_user.numtelUser, statut="en attente"
+    ).first()
+    if not reservation:
+        flash("Aucune réservation en cours.", "error")
+        return redirect(url_for("voir_panier"))
+
+    item = ContenirF.query.filter_by(idR=reservation.idR, idF=idF).first()
+    if not item:
+        flash("Cette formule n’est pas dans votre panier.", "error")
+        return redirect(url_for("voir_panier"))
+
+    formule = Formule.query.get(idF)
+    variableChoixCom = 1
+    if not reservation.sur_place :
+        variableChoixCom= 0.8
+
+    if action == "ajouter":
+        nouvelle_quantite = item.quantiteF + 1
+        for c in formule.plats:
+            plat = Plat.query.get(c.idP)
+            qte_totale = (item.quantiteF + 1) * c.quantiteC
+            if qte_totale > plat.stock or qte_totale > plat.stockInit * variableChoixCom:
+                flash(f"Pas assez de stock pour le plat '{plat.nomP}' de la formule '{formule.nomF}'", "error")
+                return redirect(url_for("voir_panier"))
+        item.quantiteF = nouvelle_quantite
+
+    elif action == "diminuer":
+        item.quantiteF -= 1
+        if item.quantiteF <= 0:
+            db.session.delete(item)
+
+    db.session.commit()
+    return redirect(url_for("voir_panier"))
+
+
+
+@app.route("/panier/valider", methods=["POST"])
+@login_required
+def valider_panier():
+    """modifie le stock et change le status de la reservation
+
+    Returns:
+        _type_: _description_
+    """
+    panier = Reservation.query.filter_by(numtelUser=current_user.numtelUser, statut="panier").first()
+    for cp in panier.plats:
+        cp.plat.stock -= cp.quantiteP
+    for cf in panier.formules:
+        for c in cf.formule.plats:
+            c.plat.stock -= cf.quantiteF * c.quantiteC
+    panier.statut = "confirmée"
+    db.session.commit()
+    flash("Réservation validée !", "success")
+    return redirect(url_for("mes_reservations"))
 
 @app.route('/admin/')
 @admin_required
