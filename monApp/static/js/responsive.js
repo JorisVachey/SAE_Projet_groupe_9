43f8ab1ec82770f7aaba8a3f3dfdf1d1632131ac
Nolan405 <nolan.morain@etu.univ-orleans.fr>
2025-11-05 11:39:32 +0100

    ajustemment du responsive pour le header + modif overflow

--- monApp/static/js/responsive.js
+++ monApp/static/js/responsive.js
@@ -1,3 +1,17 @@
+const header = document.querySelector('header');
+const sousMenu = document.querySelector('.menu');
+
+function updateMenuHeight() {
+    const isMobile = window.innerWidth <= 600;
+    if (isMobile) {
+        const headerHeight = header.getBoundingClientRect().height;
+        sousMenu.style.top = `${headerHeight}px`;
+    }
+}
+
+updateMenuHeight();
+
+
 const btnHamburger = document.getElementById('btn_hamburger');
 const menu = document.getElementById('menu');
 
