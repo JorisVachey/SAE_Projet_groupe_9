3cc82abfbe5dc489dd39662fe06b6973fcd6685a
Nolan405 <nolan.morain@etu.univ-orleans.fr>
2025-11-07 16:02:34 +0100

    avancemment des animations

--- monApp/static/js/animation.js
+++ monApp/static/js/animation.js
@@ -1,16 +1,29 @@
 window.onload = function() {
-    const faitMaison = document.querySelector('.fait_maison');
-    faitMaison.style.opacity = 1;
-    faitMaison.style.transform = 'scale(1)';
+  const faitMaison = document.querySelector('.fait_maison');
+  faitMaison.style.opacity = 1;
+  faitMaison.style.transform = 'scale(1)';
 
-    const cercle = document.querySelector('.cercle');
-    cercle.style.opacity = 1;
-    cercle.style.transform = 'scale(1)';
-    
-    
-      setTimeout(() => {
-        const header = document.querySelector('header');
-        header.style.opacity = 1;
-        header.style.transform = 'translateY(0)';
-      }, 500);
+  const cercle = document.querySelector('.cercle');
+  cercle.style.opacity = 1;
+  cercle.style.transform = 'scale(1)';
+
+  setTimeout(() => {
+    const header = document.querySelector('header');
+    header.style.opacity = 1;
+    header.style.transform = 'translateY(0)';
+  }, 500);
+
+
+
+  let dernierScroll = 0;
+  const header = document.querySelector('header');
+  window.addEventListener('scroll', () => {
+    const ScrollActuel = window.scrollY;
+    if (ScrollActuel > dernierScroll) {
+      header.classList.add('shrink');
+    } else {
+      header.classList.remove('shrink');
+    }
+    dernierScroll = ScrollActuel;
+  });
 }
