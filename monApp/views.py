4b593ccbfe74f61e71420e354cd2d76974cc9155
Nolan405 <nolan.morain@etu.univ-orleans.fr>
2026-02-11 20:38:11 +0100

    annulation de la refonte css de la partie client par manque de temmps

--- monApp/views.py
+++ monApp/views.py
@@ -140,7 +140,7 @@ def contact():
         email = request.form["email"]
         message = request.form["message"]
         msg = Message(
-            subject=f"Nouveau message de {email or "anonyme"}",
+            subject=f"Nouveau message de {email or 'anonyme'}",
             sender=app.config[
                 "MAIL_DEFAULT_SENDER"],
                 # fonctionne car on s'envoie le mail a nous meme
